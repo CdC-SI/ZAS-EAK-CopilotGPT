34b81e442c00c4f4105c60f6bb4ea4e89c287bed
Hongyi <hongyi.shi@epfl.ch>
2024-05-13 18:54:14 +0200

    sphinx doc structured (#161 #103 #89)

--- source/conf.py
+++ source/conf.py
@@ -12,7 +12,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../api'))
 sys.path.insert(0, os.path.abspath('..'))
 
 
@@ -50,9 +50,10 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_favicon = "_static/favicon.ico"
---
+++ source/intro/features.rst
@@ -0,0 +1,5 @@
+Features
+########
+
+* **Automation of Routine Tasks:** Reduces monotonous research tasks and the finding of information, allowing employees to focus on more important tasks.
+* **Decision Support:** Provides real-time assistance in decision-making through advanced algorithms.
