851879d2f7f67c1d5258d7db492cee98bcf4a9f7
kieran.schubert <schubert.kieran@gmail.com>
2024-05-22 22:32:11 +0200

    add validation check of env vars

--- config/base_config.py
+++ config/base_config.py
@@ -2,6 +2,9 @@ import os
 from dotenv import load_dotenv
 import yaml
 
+# Load check_env_vars function
+from utils.check_env import check_env_vars
+
 # Load environment variables from .env file
 load_dotenv()
 
@@ -11,6 +14,9 @@ CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'config.yaml')
 # Load the YAML configuration file
 with open(CONFIG_PATH, 'r') as file:
     config = yaml.safe_load(file)
+
+# Check env variable values from config/config.yaml
+check_env_vars(config)
 
 # Access configuration values
 autocomplete_config = config.get('autocomplete')
