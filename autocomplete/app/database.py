d18e3e2ebd628e7fbd3e536df3426c4337a7ee3b
Hongyi <hongyi.shi@epfl.ch>
2024-06-04 13:45:24 +0200

    update

--- autocomplete/app/database.py
+++ autocomplete/app/database.py
@@ -1,6 +1,6 @@
 
 import asyncpg
-from config import DB_PARAMS
+from config.db_config import DB_PARAMS
 
 
 def connect() -> asyncpg.connection:
@@ -25,3 +25,7 @@ async def fetch(query: str):
 def exact_condition(question: str):
     search_query = f"%{question.lower()}%"
     return f"LOWER(question) LIKE {search_query}"
+
+
+def fuzzy_condition(question: str, threshold: int = 5):
+    return f"levenshtein_less_equal(question, {question}, {threshold}"
