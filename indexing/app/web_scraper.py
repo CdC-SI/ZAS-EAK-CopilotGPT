5eb1231273809da1314139d5ac7fef4b6e9ac12c
Hongyi <hongyi.shi@epfl.ch>
2024-06-06 12:27:37 +0200

    refactor webscraper and added proxy handler

--- indexing/app/web_scraper.py
+++ indexing/app/web_scraper.py
@@ -3,46 +3,63 @@ from bs4 import BeautifulSoup
 from typing import List, Optional
 import logging
 from lxml import etree
+import re
 
-import queries
+from urllib3.exceptions import InsecureRequestWarning
+from urllib3 import disable_warnings
+
+if __name__ != '__main__':
+    import queries
+
 
 class WebScraper:
-    def __init__(self, base_url: str):
+    def __init__(self, base_url: str, proxy: str = ''):
         self.base_url = base_url
         self.session = requests.Session()
         self.logger = logging.getLogger(self.__class__.__name__)
+        
+        if proxy:
+            disable_warnings(category=InsecureRequestWarning)
+            self.session.verify = False
+            self.session.proxies.update({"http": proxy})
+            self.session.proxies.update({"https": proxy})
 
-    async def run(self, from_main: bool = False):
+        self.answer = {
+            'de': 'Antwort\n',
+            'it': 'Rispondi\n',
+            'fr': 'Réponse\n'
+        }
+
+    async def run(self, test: int = 0):
         """
         Asynchronously retrieves and processes FAQ data from 'https://faq.bsv.admin.ch' to insert into the database.
 
         The endpoint 'https://faq.bsv.admin.ch/sitemap.xml' is utilized to discover all relevant FAQ URLs. For each URL,
-        the method extracts the primary question (denoted by the 'h1' tag) and its corresponding answer (within an 'article' tag).
+        the method extracts the primary question (denoted by the 'h1' tag) and its corresponding answer (within an
+        'article' tag).
         Unnecessary boilerplate text will be removed for clarity and conciseness.
 
-        Each extracted FAQ entry is then upserted (inserted or updated if already exists) into the database, with detailed
-        logging to track the operation's progress and identify any errors.
+        Each extracted FAQ entry is then upserted (inserted or updated if already exists) into the database, with
+        detailed logging to track the operation's progress and identify any errors.
 
         Returns a confirmation message upon successful completion of the process.
 
         TODO:
-        - Consider implementing error handling at a more granular level to retry failed insertions or updates, enhancing the robustness of the data ingestion process.
-        - Explore optimization opportunities in text extraction and processing to improve efficiency and reduce runtime, especially for large sitemaps.
+        - Consider implementing error handling at a more granular level to retry failed insertions or updates, enhancing
+        the robustness of the data ingestion process.
+        - Explore optimization opportunities in text extraction and processing to improve efficiency and reduce runtime,
+        especially for large sitemaps.
         """
         self.logger.info(f"Beginne Datenextraktion für: {self.base_url}")
         urls = self.get_sitemap_urls()
 
-        if from_main:
-            urls = urls[:9]
+        if test:
+            urls = urls[:test]
 
         for url in urls:
-            h1, article, lang = self.extract(url)
+            lang, h1, article = self.extract_article(url)
 
-            # Efficient text processing
-            for term in ['Antwort\n', 'Rispondi\n', 'Réponse\n']:
-                article = article.replace(term, '')
-
-            if from_main:
+            if h1 and test:
                 self.logger.info("--------------------")
                 self.logger.info(f"url: {url}")
                 self.logger.info(f"question: {h1}")
@@ -57,7 +74,7 @@ class WebScraper:
         self.logger.info(f"Done! {len(urls)} wurden verarbeitet.")
         return urls
 
-    def get_response(self, url: str, timeout: int = 10) -> Optional[requests.Response]:
+    def _get_response(self, url: str, timeout: int = 10) -> Optional[requests.Response]:
         """Send a GET request and return the response object."""
         try:
             response = self.session.get(url, timeout=timeout)
@@ -69,58 +86,57 @@ class WebScraper:
 
     def get_sitemap_urls(self) -> List[str]:
         """Extract URLs from the sitemap."""
-        response = self.get_response(self.base_url)
-        if not response:
-            return []
+        response = self._get_response(self.base_url)
 
-        root = etree.fromstring(response.content)
-        namespace = {'sitemap': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
-        urls = [url.text for url in root.xpath("//sitemap:loc", namespaces=namespace)]
+        path = []
+        if response is not None:
+            root = etree.fromstring(response.content)
+            namespace = {'sitemap': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
+            path = root.xpath("//sitemap:loc", namespaces=namespace)
+        urls = [url.text for url in path]
 
         return urls
 
-    def extract(self, url: str):
-        response = self.get_response(url)
+    def extract_article(self, url: str):
+        response = self._get_response(url)
         if not response:
             return ''
 
         soup = BeautifulSoup(response.text, 'lxml')
 
         extracted = []
-        for tag in ['h1', 'article', 'html']:
+        for tag in ['html', 'h1', 'article']:
             element = soup.find(tag)
-
-            if tag == 'html':
-                extracted += element['lang'] if element and element.has_attr('lang') else ''
+            
+            if not element:
+                extracted.append('')
+            elif tag == 'html':
+                extracted.append(element['lang'] if element.has_attr('lang') else '')
             else:
-                extracted += '\n'.join(line.strip() for line in element.get_text().splitlines() if line) if element else ''
+                text = element.get_text()
+                if tag == 'article':
+                    text = text.replace(self.answer.get(extracted[0], ''), '')
+                text = re.sub(r"((\r\n|\r|\n)\s*){2,}", "\n\n", text.strip())
+                extracted.append(text)
 
         return extracted[0], extracted[1], extracted[2]
 
-    def extract_text_from_tag(self, url: str, tag: str) -> str:
-        """Extract text from a specific tag in a web page."""
-        response = self.get_response(url)
-        if not response:
-            return ''
-
-        soup = BeautifulSoup(response.text, 'lxml')
-        body = soup.find(tag)
-        return '\n'.join(line.strip() for line in body.get_text().splitlines() if line) if body else ''
-
-    def detect_language(self, url: str) -> str:
-        """Detect the language of a website based on the 'lang' attribute in the HTML tag."""
-        response = self.get_response(url)
-        if not response:
-            return ''
-
-        soup = BeautifulSoup(response.text, 'lxml')
-        html_tag = soup.find('html')
-        return html_tag['lang'] if html_tag and html_tag.has_attr('lang') else ''
-
 
 if __name__ == '__main__':
+    import argparse
+    import asyncio
+
+    parser = argparse.ArgumentParser(description='Run WebScraper demo')
+    parser.add_argument('--sitemap', type=str,
+                        default='https://faq.bsv.admin.ch/sitemap.xml',
+                        help='The sitemap URL of the website to scrape (default: https://faq.bsv.admin.ch/sitemap.xml)')
+    # noinspection HttpUrlsUsage
+    parser.add_argument('--proxy', type=str,
+                        default='',
+                        help='The proxy address if you are using one (example: http://your-proxy-url.com:0000')
+    args = parser.parse_args()
+
     logging.basicConfig(level=logging.INFO)
-    
-    sitemap_url = 'https://faq.bsv.admin.ch/sitemap.xml'
-    scraper = WebScraper(sitemap_url)
-    scraper.run(True)
+
+    scraper = WebScraper(args.sitemap, args.proxy)
+    asyncio.run(scraper.run(9))
