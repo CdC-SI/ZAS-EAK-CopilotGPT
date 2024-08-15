29d3fd606a1177d55aa4d6b1940baed5f99033f5
kieran.schubert <schubert.kieran@gmail.com>
2024-08-15 13:42:28 +0200

    update type hints

--- src/copilot/app/rag/base.py
+++ src/copilot/app/rag/base.py
@@ -1,6 +1,8 @@
+from typing import List
+from database.models import Document
 from abc import ABC, abstractmethod
 
 class BaseRetriever(ABC):
     @abstractmethod
-    def get_documents(self):
+    def get_documents(self, query: str) -> List[Document]:
         pass
