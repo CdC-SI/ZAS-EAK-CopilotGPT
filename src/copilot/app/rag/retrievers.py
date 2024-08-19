2c9a52caa1d76d3495440ae863c16695bdfc448c
kieran.schubert <schubert.kieran@gmail.com>
2024-08-19 11:16:05 +0200

    implement cohere reranker

--- src/copilot/app/rag/retrievers.py
+++ src/copilot/app/rag/retrievers.py
@@ -18,83 +18,6 @@ load_dotenv()
 # Load Cohere API key
 COHERE_API_KEY = os.environ["COHERE_API_KEY"]
 
-# class RetrieverClient(BaseRetriever):
-#     """
-#     A client for retrieving documents using multiple retrieval strategies in parallel.
-
-#     The `RetrieverClient` class manages a collection of retrievers and executes their
-#     `get_documents` methods in parallel, aggregating the results into a single list of documents.
-
-#     Parameters
-#     ----------
-#     retrievers : list
-#         A list of retriever instances that implement the `get_documents` method. These retrievers
-#         are executed in parallel to retrieve documents based on the specified query.
-
-#     Methods
-#     -------
-#     get_documents(db, query, language, k)
-#         Retrieves documents from the database using the provided query, language and returns top k documents. The results are aggregated into a single list of documents.
-
-#     """
-#     def __init__(self, retrievers):
-#         #self.retrievers = retrievers
-#         self.retrievers = [r for r in retrievers if not isinstance(r, Reranker)]
-#         self.reranker = next((r for r in retrievers if isinstance(r, Reranker)), None)
-
-
-#     def get_documents(self, db, query, language, k) -> List[Document]:
-#         """
-#         Retrieve documents using multiple retrievers in parallel.
-
-#         This method executes the `get_documents` method of each retriever in parallel using a
-#         ThreadPoolExecutor. The results from all retrievers are aggregated into a single list,
-#         which is then returned. If any retriever raises an exception, it is caught and logged,
-#         but the retrieval process continues for the remaining retrievers.
-
-#         Parameters
-#         ----------
-#         db : Any
-#             The database connection or session object used by the retrievers to query documents.
-#         query : str
-#             The search query used to retrieve relevant documents.
-#         language : str
-#             The language in which the documents are retrieved.
-#         k : int
-#             The number of top documents to retrieve from each retriever.
-
-#         Returns
-#         -------
-#         docs : list
-#             A list of documents retrieved from the database, aggregated from all the retrievers.
-
-#         Raises
-#         ------
-#         None
-#             Exceptions raised by individual retrievers are caught and logged, not propagated.
-#         """
-#         docs = []
-
-#         with ThreadPoolExecutor() as executor: # Use ThreadPoolExecutor for parallel execution
-#             future_to_retriever = {
-#                 executor.submit(retriever.get_documents, db, query, language, k): retriever
-#                 for retriever in self.retrievers
-#             }
-
-#             for future in as_completed(future_to_retriever): # Collect results as they complete
-#                 retriever = future_to_retriever[future]
-#                 try:
-#                     result = future.result()
-#                     docs.extend(result)
-#                 except Exception as e:
-#                     print(f"Retriever {retriever} raised an exception: {e}")
-
-#         # Remove duplicate documents
-#         seen = set()
-#         unique_docs = [doc for doc in docs if doc.id not in seen and not seen.add(doc.id)]
-
-#         return unique_docs[:k]
-
 class RetrieverClient(BaseRetriever):
     """
     A client for retrieving documents using multiple retrieval strategies in parallel.
@@ -115,8 +38,6 @@ class RetrieverClient(BaseRetriever):
 
     """
     def __init__(self, retrievers, reranker):
-        # self.retrievers = [r for r in retrievers if not isinstance(r, Reranker)]
-        # self.reranker = next((r for r in retrievers if isinstance(r, Reranker)), None)
         self.retrievers = retrievers
         self.reranker = reranker
 
@@ -168,16 +89,19 @@ class RetrieverClient(BaseRetriever):
 
         # Remove duplicate documents
         seen = set()
-        unique_docs = [doc.text for doc in docs if doc.id not in seen and not seen.add(doc.id)]
-        print("-----------UNIQUEDOCS: ", unique_docs[:3])
-        #unique_docs = [Document.from_orm(doc) for doc in docs if doc.id not in seen and not seen.add(doc.id)]
+        unique_docs = [Document.from_orm(doc) for doc in docs if doc.id not in seen and not seen.add(doc.id)]
 
         if self.reranker:
-            reranked_docs = self.reranker.rerank(query, unique_docs)
-            return reranked_docs[:k]
+            try:
+                rerank_res = self.reranker.rerank(query, unique_docs).results
+                rerank_idx = [res.index for res in rerank_res]
+                reranked_docs = [unique_docs[i] for i in rerank_idx]
+                return reranked_docs[:k]
+            except Exception as e:
+                print(f"Reranker raised an exception: {e}")
+                return unique_docs[:k]
 
         return unique_docs[:k]
-
 
 class TopKRetriever(BaseRetriever):
     """
@@ -527,7 +451,7 @@ class BM25Retriever(BaseRetriever):
         # # compute bm25 score
         scores = self.bm25_score(query, docs)
 
-        # # sort retrieved context docs according to score
+        # sort retrieved context docs according to score
         top_docs = list(sorted(zip(docs, scores), key=lambda x: x[1], reverse=True))[:self.top_k]
 
         docs = [Document(id=doc[0].id,
@@ -543,8 +467,7 @@ class Reranker():
         self.model = model
         self.top_k = top_k
 
-    #def rerank(self, query: str, documents: List[Document]) -> List[Document]:
-    def rerank(self, query: str, documents: List[str]):
+    def rerank(self, query: str, documents: List[Document]) -> List[Document]:
 
         reranked_docs = self.reranking_client.rerank(
             model=self.model,
@@ -552,8 +475,5 @@ class Reranker():
             documents=documents,
             top_n=self.top_k,
         )
-
-        print("------ Reranked Documents ------: ", reranked_docs)
-        print(reranked_docs[0].document)
 
         return reranked_docs
