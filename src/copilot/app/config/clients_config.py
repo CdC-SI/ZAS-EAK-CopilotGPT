79d499da54bb629ee7b39f4fb50cc6d0b11f1f06
Hongyi <61227237+Shi-Ho@users.noreply.github.com>
2024-08-20 18:21:07 +0200

    Update clients_config.py

--- src/copilot/app/config/clients_config.py
+++ src/copilot/app/config/clients_config.py
@@ -5,9 +5,11 @@ import openai
 import cohere
 
 import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 from config.base_config import rag_config
-from config.llm_config import SUPPORTED_OPENAI_LLM_MODELS, SUPPORTED_GROQ_LLM_MODELS
+from config.llm_config import SUPPORTED_GROQ_LLM_MODELS
 
 load_dotenv()
 
@@ -19,25 +21,22 @@ COHERE_API_KEY = os.environ.get("COHERE_API_KEY", None)
 # Load Proxy settings
 HTTP_PROXY = os.environ.get("HTTP_PROXY", None)
 REQUESTS_CA_BUNDLE = os.environ.get("REQUESTS_CA_BUNDLE", None)
-
-# Create API clients
-clientEmbed = openai.OpenAI(api_key=OPENAI_API_KEY)
-clientLLM = clientEmbed  # default LLM client
-clientRerank = cohere.Client(api_key=COHERE_API_KEY)
-
-if rag_config["llm"]["model"] in SUPPORTED_GROQ_LLM_MODELS:
-    from groq import Groq
-    clientLLM = Groq(api_key=GROQ_API_KEY)
+logger.info(f"HTTP_PROXY: {HTTP_PROXY}, REQUESTS_CA_BUNDLE: {REQUESTS_CA_BUNDLE}")
 
 # if HTTP_PROXY then set the proxy in openai client
+httpx_client = None
 if HTTP_PROXY and REQUESTS_CA_BUNDLE:
-    logger = logging.getLogger(__name__)
     logger.info(f"Setting up HTTP_PROXY: {HTTP_PROXY}")
     logger.info(f"Setting up REQUESTS_CA_BUNDLE: {REQUESTS_CA_BUNDLE}")
 
     import httpx
     httpx_client = httpx.Client(proxy=HTTP_PROXY, verify=REQUESTS_CA_BUNDLE)
 
-    clientEmbed.http_client = httpx.Client(proxy=HTTP_PROXY, verify=REQUESTS_CA_BUNDLE)
-    clientLLM.http_client = httpx.Client(proxy=HTTP_PROXY, verify=REQUESTS_CA_BUNDLE)
-    clientRerank.http_client = httpx.Client(proxy=HTTP_PROXY, verify=REQUESTS_CA_BUNDLE)
+# Create API clients
+clientEmbed = openai.OpenAI(api_key=OPENAI_API_KEY, http_client=httpx_client)
+clientLLM = clientEmbed  # default LLM client
+clientRerank = cohere.Client(api_key=COHERE_API_KEY, httpx_client=httpx_client)
+
+if rag_config["llm"]["model"] in SUPPORTED_GROQ_LLM_MODELS:
+    from groq import Groq
+    clientLLM = Groq(api_key=GROQ_API_KEY, http_client=httpx_client)
