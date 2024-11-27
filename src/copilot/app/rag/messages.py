767863414e60a14d304714ce972ae15e0a6dac60
kieran.schubert <schubert.kieran@gmail.com>
2024-11-27 16:44:09 +0100

    refactor message handler + add MLX support

--- src/copilot/app/rag/messages.py
+++ src/copilot/app/rag/messages.py
@@ -1,5 +1,5 @@
-from typing import List, Dict
-from rag.prompts import OPENAI_RAG_SYSTEM_PROMPT_DE, OPENAI_RAG_SYSTEM_PROMPT_FR, OPENAI_RAG_SYSTEM_PROMPT_IT, QUERY_REWRITING_PROMPT_DE, QUERY_REWRITING_PROMPT_FR, QUERY_REWRITING_PROMPT_IT, CONTEXTUAL_COMPRESSION_PROMPT_DE, CONTEXTUAL_COMPRESSION_PROMPT_FR, CONTEXTUAL_COMPRESSION_PROMPT_IT, CREATE_CHAT_TITLE_PROMPT_DE, CREATE_CHAT_TITLE_PROMPT_FR, CREATE_CHAT_TITLE_PROMPT_IT, SUMMARIZE_COMMAND_PROMPT_DE, SUMMARIZE_COMMAND_PROMPT_FR, SUMMARIZE_COMMAND_PROMPT_IT, EXPLAIN_COMMAND_PROMPT_DE, EXPLAIN_COMMAND_PROMPT_FR, EXPLAIN_COMMAND_PROMPT_IT
+from typing import List, Dict, Union
+from rag.prompts import RAG_SYSTEM_PROMPT_DE, RAG_SYSTEM_PROMPT_FR, RAG_SYSTEM_PROMPT_IT, QUERY_REWRITING_PROMPT_DE, QUERY_REWRITING_PROMPT_FR, QUERY_REWRITING_PROMPT_IT, CONTEXTUAL_COMPRESSION_PROMPT_DE, CONTEXTUAL_COMPRESSION_PROMPT_FR, CONTEXTUAL_COMPRESSION_PROMPT_IT, CREATE_CHAT_TITLE_PROMPT_DE, CREATE_CHAT_TITLE_PROMPT_FR, CREATE_CHAT_TITLE_PROMPT_IT, SUMMARIZE_COMMAND_PROMPT_DE, SUMMARIZE_COMMAND_PROMPT_FR, SUMMARIZE_COMMAND_PROMPT_IT, EXPLAIN_COMMAND_PROMPT_DE, EXPLAIN_COMMAND_PROMPT_FR, EXPLAIN_COMMAND_PROMPT_IT
 from config.llm_config import SUPPORTED_OPENAI_LLM_MODELS, SUPPORTED_AZUREOPENAI_LLM_MODELS, SUPPORTED_ANTHROPIC_LLM_MODELS, SUPPORTED_GROQ_LLM_MODELS
 
 from langfuse.decorators import observe
@@ -11,62 +11,62 @@ class MessageBuilder:
         self.llm_model = llm_model
 
     @observe()
-    def build_chat_prompt(self, context_docs: List[Dict], query: str, conversational_memory: str) -> List[Dict]:
+    def build_chat_prompt(self, context_docs: List[Dict], query: str, conversational_memory: str) -> Union[List[Dict], str]:
         """
         Format the RAG message to send to the appropriate LLM API.
         """
+        # NEED TO IMPLEMENT OPTIMIZED PROMPTS per model and provider
         # For OpenAI LLM models
-        if self.llm_model in SUPPORTED_OPENAI_LLM_MODELS + SUPPORTED_AZUREOPENAI_LLM_MODELS:
+        if self.llm_model in SUPPORTED_OPENAI_LLM_MODELS + SUPPORTED_AZUREOPENAI_LLM_MODELS + SUPPORTED_GROQ_LLM_MODELS:
             if self.language == "de":
-                prompt = OPENAI_RAG_SYSTEM_PROMPT_DE.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
+                prompt = RAG_SYSTEM_PROMPT_DE.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
                 return [{"role": "system", "content": prompt},]
             elif self.language == "fr":
-                prompt = OPENAI_RAG_SYSTEM_PROMPT_FR.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
+                prompt = RAG_SYSTEM_PROMPT_FR.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
                 return [{"role": "system", "content": prompt},]
             elif self.language == "it":
-                prompt = OPENAI_RAG_SYSTEM_PROMPT_IT.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
+                prompt = RAG_SYSTEM_PROMPT_IT.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
                 return [{"role": "system", "content": prompt},]
             else:
-                prompt = OPENAI_RAG_SYSTEM_PROMPT_DE.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
+                prompt = RAG_SYSTEM_PROMPT_DE.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
                 return [{"role": "system", "content": prompt},]
 
-        # NEED TO IMPLEMENT OPTIMIZED ANTHROPIC PROMPT
         elif self.llm_model in SUPPORTED_ANTHROPIC_LLM_MODELS:
             if self.language == "de":
-                prompt = OPENAI_RAG_SYSTEM_PROMPT_DE.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
+                prompt = RAG_SYSTEM_PROMPT_DE.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
                 return [{"role": "user", "content": prompt},]
             elif self.language == "fr":
-                prompt = OPENAI_RAG_SYSTEM_PROMPT_FR.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
+                prompt = RAG_SYSTEM_PROMPT_FR.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
                 return [{"role": "user", "content": prompt},]
             elif self.language == "it":
-                prompt = OPENAI_RAG_SYSTEM_PROMPT_IT.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
+                prompt = RAG_SYSTEM_PROMPT_IT.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
                 return [{"role": "user", "content": prompt},]
             else:
-                prompt = OPENAI_RAG_SYSTEM_PROMPT_DE.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
+                prompt = RAG_SYSTEM_PROMPT_DE.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
                 return [{"role": "user", "content": prompt},]
 
-        # NEED TO IMPLEMENT OPTIMIZED GROQ PROMPT
-        elif self.llm_model in SUPPORTED_GROQ_LLM_MODELS:
+        elif self.llm_model.startswith("mlx-community/"):
             if self.language == "de":
-                prompt = OPENAI_RAG_SYSTEM_PROMPT_DE.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
-                return [{"role": "system", "content": prompt},]
+                prompt = RAG_SYSTEM_PROMPT_DE.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
+                return prompt
             elif self.language == "fr":
-                prompt = OPENAI_RAG_SYSTEM_PROMPT_FR.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
-                return [{"role": "system", "content": prompt},]
+                prompt = RAG_SYSTEM_PROMPT_FR.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
+                return prompt
             elif self.language == "it":
-                prompt = OPENAI_RAG_SYSTEM_PROMPT_IT.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
-                return [{"role": "system", "content": prompt},]
+                prompt = RAG_SYSTEM_PROMPT_IT.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
+                return prompt
             else:
-                prompt = OPENAI_RAG_SYSTEM_PROMPT_DE.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
-                return [{"role": "system", "content": prompt},]
+                prompt = RAG_SYSTEM_PROMPT_DE.format(context_docs=context_docs, query=query, conversational_memory=conversational_memory)
+                return prompt
 
     @observe()
     def build_query_rewriting_prompt(self, n_alt_queries: int, query: str) -> List[Dict]:
         """
         Format the Query Rewriting message to send to the appropriate LLM API.
         """
+        # NEED TO IMPLEMENT OPTIMIZED PROMPTS per model and provider
         # For OpenAI LLM models
-        if self.llm_model in SUPPORTED_OPENAI_LLM_MODELS + SUPPORTED_AZUREOPENAI_LLM_MODELS:
+        if self.llm_model in SUPPORTED_OPENAI_LLM_MODELS + SUPPORTED_AZUREOPENAI_LLM_MODELS + SUPPORTED_GROQ_LLM_MODELS:
             if self.language == "de":
                 prompt = QUERY_REWRITING_PROMPT_DE.format(n_alt_queries=n_alt_queries, query=query)
                 return [{"role": "system", "content": prompt},]
@@ -95,20 +95,19 @@ class MessageBuilder:
                 prompt = QUERY_REWRITING_PROMPT_DE.format(n_alt_queries=n_alt_queries, query=query)
                 return [{"role": "user", "content": prompt},]
 
-        # NEED TO IMPLEMENT OPTIMIZED GROQ PROMPT
-        elif self.llm_model in SUPPORTED_GROQ_LLM_MODELS:
+        elif self.llm_model.startswith("mlx-community/"):
             if self.language == "de":
                 prompt = QUERY_REWRITING_PROMPT_DE.format(n_alt_queries=n_alt_queries, query=query)
-                return [{"role": "system", "content": prompt},]
+                return prompt
             elif self.language == "fr":
                 prompt = QUERY_REWRITING_PROMPT_FR.format(n_alt_queries=n_alt_queries, query=query)
-                return [{"role": "system", "content": prompt},]
+                return prompt
             elif self.language == "it":
                 prompt = QUERY_REWRITING_PROMPT_IT.format(n_alt_queries=n_alt_queries, query=query)
-                return [{"role": "system", "content": prompt},]
+                return prompt
             else:
                 prompt = QUERY_REWRITING_PROMPT_DE.format(n_alt_queries=n_alt_queries, query=query)
-                return [{"role": "system", "content": prompt},]
+                return prompt
 
     @observe()
     def build_contextual_compression_prompt(self, context_doc: str, query: str) -> List[Dict]:
@@ -116,7 +115,7 @@ class MessageBuilder:
         Format the Contextual Compression message to send to the appropriate LLM API.
         """
         # For OpenAI LLM models
-        if self.llm_model in SUPPORTED_OPENAI_LLM_MODELS + SUPPORTED_AZUREOPENAI_LLM_MODELS:
+        if self.llm_model in SUPPORTED_OPENAI_LLM_MODELS + SUPPORTED_AZUREOPENAI_LLM_MODELS + SUPPORTED_GROQ_LLM_MODELS:
             if self.language == "de":
                 prompt = CONTEXTUAL_COMPRESSION_PROMPT_DE.format(context_doc=context_doc, query=query)
                 return [{"role": "system", "content": prompt},]
@@ -145,20 +144,19 @@ class MessageBuilder:
                 prompt = CONTEXTUAL_COMPRESSION_PROMPT_DE.format(context_doc=context_doc, query=query)
                 return [{"role": "user", "content": prompt},]
 
-        # NEED TO IMPLEMENT OPTIMIZED GROQ PROMPT
-        elif self.llm_model in SUPPORTED_GROQ_LLM_MODELS:
+        elif self.llm_model.startswith("mlx-community/"):
             if self.language == "de":
                 prompt = CONTEXTUAL_COMPRESSION_PROMPT_DE.format(context_doc=context_doc, query=query)
-                return [{"role": "system", "content": prompt},]
+                return prompt
             elif self.language == "fr":
                 prompt = CONTEXTUAL_COMPRESSION_PROMPT_FR.format(context_doc=context_doc, query=query)
-                return [{"role": "system", "content": prompt},]
+                return prompt
             elif self.language == "it":
                 prompt = CONTEXTUAL_COMPRESSION_PROMPT_IT.format(context_doc=context_doc, query=query)
-                return [{"role": "system", "content": prompt},]
+                return prompt
             else:
                 prompt = CONTEXTUAL_COMPRESSION_PROMPT_DE.format(context_doc=context_doc, query=query)
-                return [{"role": "system", "content": prompt},]
+                return prompt
 
     @observe()
     def build_chat_title_prompt(self, query: str, assistant_response: str) -> List[Dict]:
@@ -166,7 +164,7 @@ class MessageBuilder:
         Format the CreateChatTitle message to send to the appropriate LLM API.
         """
         # For OpenAI LLM models
-        if self.llm_model in SUPPORTED_OPENAI_LLM_MODELS + SUPPORTED_AZUREOPENAI_LLM_MODELS:
+        if self.llm_model in SUPPORTED_OPENAI_LLM_MODELS + SUPPORTED_AZUREOPENAI_LLM_MODELS + SUPPORTED_GROQ_LLM_MODELS:
             if self.language == "de":
                 prompt = CREATE_CHAT_TITLE_PROMPT_DE.format(query=query, assistant_response=assistant_response)
                 return [{"role": "system", "content": prompt},]
@@ -195,20 +193,19 @@ class MessageBuilder:
                 prompt = CREATE_CHAT_TITLE_PROMPT_DE.format(query=query, assistant_response=assistant_response)
                 return [{"role": "user", "content": prompt},]
 
-        # NEED TO IMPLEMENT OPTIMIZED GROQ PROMPT
-        elif self.llm_model in SUPPORTED_GROQ_LLM_MODELS:
+        elif self.llm_model.startswith("mlx-community/"):
             if self.language == "de":
                 prompt = CREATE_CHAT_TITLE_PROMPT_DE.format(query=query, assistant_response=assistant_response)
-                return [{"role": "system", "content": prompt},]
+                return prompt
             elif self.language == "fr":
                 prompt = CREATE_CHAT_TITLE_PROMPT_FR.format(query=query, assistant_response=assistant_response)
-                return [{"role": "system", "content": prompt},]
+                return prompt
             elif self.language == "it":
                 prompt = CREATE_CHAT_TITLE_PROMPT_IT.format(query=query, assistant_response=assistant_response)
-                return [{"role": "system", "content": prompt},]
+                return prompt
             else:
                 prompt = CREATE_CHAT_TITLE_PROMPT_DE.format(query=query, assistant_response=assistant_response)
-                return [{"role": "system", "content": prompt},]
+                return prompt
 
     @observe()
     def build_summarize_prompt(self, command: str, input_text: str, mode: str, style: str) -> List[Dict]:
@@ -216,7 +213,7 @@ class MessageBuilder:
         Format the SummarizeCommand message to send to the appropriate LLM API.
         """
         # For OpenAI LLM models
-        if self.llm_model in SUPPORTED_OPENAI_LLM_MODELS + SUPPORTED_AZUREOPENAI_LLM_MODELS:
+        if self.llm_model in SUPPORTED_OPENAI_LLM_MODELS + SUPPORTED_AZUREOPENAI_LLM_MODELS + SUPPORTED_GROQ_LLM_MODELS:
             if command == "/summarize":
                 if self.language == "de":
                     prompt = SUMMARIZE_COMMAND_PROMPT_DE.format(input_text=input_text, mode=mode, style=style)
@@ -231,4 +228,33 @@ class MessageBuilder:
                     prompt = SUMMARIZE_COMMAND_PROMPT_DE.format(input_text=input_text, mode=mode, style=style)
                     return [{"role": "system", "content": prompt},]
 
-        # TO DO: IMPLEMENT FOR GROQ + ANTHROPIC + GEMINI
+        # For Anthropic LLM models
+        if self.llm_model in SUPPORTED_ANTHROPIC_LLM_MODELS:
+            if command == "/summarize":
+                if self.language == "de":
+                    prompt = SUMMARIZE_COMMAND_PROMPT_DE.format(input_text=input_text, mode=mode, style=style)
+                    return [{"role": "user", "content": prompt},]
+                elif self.language == "fr":
+                    prompt = SUMMARIZE_COMMAND_PROMPT_FR.format(input_text=input_text, mode=mode, style=style)
+                    return [{"role": "user", "content": prompt},]
+                elif self.language == "it":
+                    prompt = SUMMARIZE_COMMAND_PROMPT_IT.format(input_text=input_text, mode=mode, style=style)
+                    return [{"role": "user", "content": prompt},]
+                else:
+                    prompt = SUMMARIZE_COMMAND_PROMPT_DE.format(input_text=input_text, mode=mode, style=style)
+                    return [{"role": "user", "content": prompt},]
+
+        if self.llm_model.startswith("mlx-community/"):
+            if command == "/summarize":
+                if self.language == "de":
+                    prompt = SUMMARIZE_COMMAND_PROMPT_DE.format(input_text=input_text, mode=mode, style=style)
+                    return prompt
+                elif self.language == "fr":
+                    prompt = SUMMARIZE_COMMAND_PROMPT_FR.format(input_text=input_text, mode=mode, style=style)
+                    return prompt
+                elif self.language == "it":
+                    prompt = SUMMARIZE_COMMAND_PROMPT_IT.format(input_text=input_text, mode=mode, style=style)
+                    return prompt
+                else:
+                    prompt = SUMMARIZE_COMMAND_PROMPT_DE.format(input_text=input_text, mode=mode, style=style)
+                    return prompt
