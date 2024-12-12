86bc2a368b86db4712f9c1acea501be559c0b33f
kieran.schubert <schubert.kieran@gmail.com>
2024-12-12 12:43:26 +0100

    add source validation schema

--- src/copilot/app/schemas/agents.py
+++ src/copilot/app/schemas/agents.py
@@ -1,5 +1,17 @@
+from typing import List
 from pydantic import BaseModel
 
 
 class FunctionCall(BaseModel):
     function_call: str
+
+
+class MultipleSourceValidation(BaseModel):
+    sources: List[str]
+    is_valid: bool
+
+
+class UniqueSourceValidation(BaseModel):
+    is_partial: bool
+    is_valid: bool
+    reason: str
