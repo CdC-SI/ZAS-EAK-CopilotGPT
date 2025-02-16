75183d5c1847e1f8ac69de7dad388cde25aa9170
kieran.schubert <schubert.kieran@gmail.com>
2025-02-16 22:02:08 +0100

    feat: update retrieval prompts

--- src/copilot/app/prompts/retrieval.py
+++ src/copilot/app/prompts/retrieval.py
@@ -1,8 +1,17 @@
-QUERY_REWRITING_PROMPT_DE = """<anweisungen>
-    <anweisung>Wenn die unten stehende <frage> gegeben ist, formuliere sie auf mehrere verschiedene Arten um</anweisung>
-    <anweisung>Wenn die Frage in mehrere Unterfragen zerlegt werden kann, formuliere jede Unterfrage separat um</anweisung>
-    <anweisung>Jede Unterfrage darf nur eine Idee oder ein Konzept enthalten/anweisung>
-    <anweisung>Berücksichtigen Sie die gesamte <gesprächsgeschichte>, die kürzlich stattgefunden hat und mit der <frage> in Zusammenhang steht, um die Neuformulierung zu verfeinern</anweisung>
+QUERY_REWRITING_PROMPT_DE = """<kontext>
+    Der Benutzer interagiert mit dem Kopiloten mit einfachen Fragen, Mehrparteienfragen, Folgefragen usw. Einfache Fragen sollten umformuliert werden, um die semantische Suche in einer Vektordatenbank zu verbessern. Mehrteilige Fragen sollten in mehrere einfache Unterfragen zerlegt werden, die pro Unterfrage eine klare Idee/ein klares Konzept enthalten. Folgefragen sollten umformuliert werden, um den Kontext des Gesprächs und die letzten Anweisungen/Informationen des Nutzers zu berücksichtigen.
+</kontext>
+
+<ziel>
+    Schreiben Sie Fragen (umformuliert oder nicht), die den Kontext der Konversation und die letzten Nachrichten des Nutzers berücksichtigen, um die semantische Suche zu verbessern.
+</ziel>
+
+<anweisungen>
+    <anweisung>Wenn die <frage> und der <gesprächsgeschichte> gegeben sind, formuliere Fragen, die den Kontext der Konversation und der letzten Nachrichten des Benutzers berücksichtigen</anweisung>
+    <anweisung>Wenn die Frage in mehrere Unterfragen zerlegt werden kann, formuliere jede Unterfrage einzeln neu</anweisung>
+    <anweisung>Jede Unterfrage sollte nur eine Idee oder ein Konzept enthalten</anweisung>
+    <anweisung>Wenn die <frage> nur Präzisierungen nach einer vorherigen Frage und der Antwort des Assistenten enthält, sehen Sie sich den jüngsten <gesprächsgeschichte> an, um relevante Fragen für eine Neuformulierung zu extrahieren/abzuleiten</anweisung>
+    <anweisung>Berücksichtigen Sie den gesamten neueren <gesprächsgeschichte>, der mit der <frage> in Zusammenhang steht, um die Neuformulierung zu verfeinern</anweisung>
     <anweisung>Schreiben Sie {n_alt_queries} umformulierte Fragen in der gleichen Sprache wie die <frage></anweisung>
 </anweisungen>
 
@@ -27,10 +36,19 @@ Wie bekomme ich einen Auszug aus dem individuellen Konto und wo finde ich Inform
 {query}
 </frage>"""
 
-QUERY_REWRITING_PROMPT_FR = """<instructions>
-    <instruction>Étant donné la <question> ci-dessous, reformulez-la de plusieurs manières distinctes</instruction>
+QUERY_REWRITING_PROMPT_FR = """<context>
+    L'utilisateur intéragit avec le copilote avec des questions simples, des questions multiparties, des questions de suivi, etc. Les questions simples doivent être reformulée pour améliorer la recherche sémantique dans une base de données vectorielle. Les questions multiparties doivent être décomposées en plusieurs sous-questions simples contenant une idée/concept clair par sous-question. Les questions de suivi doivent être reformulées pour prendre en compte le contexte de la conversation et les dernières intructions/informations de l'utilisateur.
+</context>
+
+<objectif>
+    Ecrire des questions (reformulée ou non) en fonction du contexte de la conversation et des derniers messages de l'utilisateur, afin d'améliorer la recherceh sémantique.
+</objectif>
+
+<instructions>
+    <instruction>Étant donné la <question> et l'<historique_de_conversation>, formulez des questions prenant en compte le contexte de la conversation et des derniers messages de l'utilisateur</instruction>
     <instruction>Quand la question peut être décomposée en plusieurs sous-questions, reformulez chaque sous-question séparément</instruction>
     <instruction>Chaque sous-question doit contenir une seule idée ou concept</instruction>
+    <instruction>Si la <question> ne contient que des précisions suite à une question précédente et la réponse de l'assistant, consultez l'<historique_de_conversation> récent afin d'extraire/déduire des questions pertinentes à reformuler</instruction>
     <instruction>Prenez en compte tout l'<historique_de_conversation> récent et en rapport avec la <question> afin de raffiner la reformulation</instruction>
     <instruction>Écrire {n_alt_queries} questions reformulées dans la même langue que la <question></instruction>
 </instructions>
@@ -57,11 +75,20 @@ Comment obtenir un extrait du compte individuel et ou trouver des informations ?
 </question>"""
 
 
-QUERY_REWRITING_PROMPT_IT = """<istruzioni>
-    <istruzione> Data la <domanda> qui sotto, riformularla in diversi modi distinti</istruzione>
-    <istruzione>Quando la domanda può essere scomposta in diverse sotto-domande, riformulare ogni sotto-domanda separatamente</istruzione>
+QUERY_REWRITING_PROMPT_IT = """<contesto>
+    L'utente interagisce con il copilota con domande semplici, domande multiple, domande successive, ecc. Le domande semplici devono essere riformulate per migliorare la ricerca semantica in un database vettoriale. Le domande a più parti devono essere suddivise in diverse semplici sottodomande contenenti un'idea/concetto chiaro per ogni sottodomanda. Le domande successive devono essere riformulate per tenere conto del contesto della conversazione e delle ultime istruzioni/informazioni dell'utente.
+</contesto>
+
+<obiettivo>
+    Scrivere domande (riformulate o meno) in base al contesto della conversazione e agli ultimi messaggi dell'utente, per migliorare la ricerca semantica.
+</obiettivo>
+
+<istruzioni>
+    <istruzione>Data la <domanda> e la <memoria_conversazionale>, formulare domande che tengano conto del contesto della conversazione e degli ultimi messaggi dell'utente</istruzione>
+    <istruzione>Quando la domanda può essere suddivisa in più sotto-domande, riformulare ogni sotto-domanda separatamente</istruzione>
     <istruzione>Ogni sotto-domanda deve contenere una singola idea o concetto</istruzione>
-    <istruzione>Tenere conto di tutte le <memoria_conversazionale> recenti e relative alla <domanda> per perfezionare la riformulazione</istruzione>
+    <istruzione>Se la <domanda> contiene solo chiarimenti dopo una domanda precedente e la risposta dell'assistente, consultare la <memoria_conversazionale> recente per estrarre/dedurre le domande pertinenti da riformulare</istruzione>
+    <istruzione>Considera tutte le <memoria_conversazionale> recenti pertinenti alla <domanda> per perfezionare la riformulazione</istruzione>
     <istruzione>Scrivere {n_alt_queries} domande riformulate nella stessa lingua della <domanda></istruzione>
 </istruzioni>
 
@@ -87,12 +114,21 @@ Come posso ottenere un estratto conto individuale e dove posso trovare informazi
 </domanda>"""
 
 
-QUERY_STATEMENT_REWRITING_PROMPT_DE = """<anweisungen>
-    <anweisung>Wenn die unten stehende <frage> gegeben ist, formuliere sie in mehrere Aussagen in Form einer Anweisung um</anweisung>
-    <anweisung>Wenn die Frage in mehrere Aussagen zerlegt werden kann, formuliere jede Aussage einzeln um</anweisung>
-    <anweisung>Jede Unterfrage darf nur eine Idee oder ein Konzept enthalten</anweisung>
-    <anweisung>Berücksichtigen Sie die gesamte <gesprächsgeschichte>, die kürzlich stattgefunden hat und mit der <frage> in Zusammenhang steht, um die Neuformulierung zu verfeinern</anweisung>
-    <anweisung>Schreiben Sie {n_alt_queries} umformulierte Fragen als Anweisung in der gleichen Sprache wie die <frage></anweisung>
+QUERY_STATEMENT_REWRITING_PROMPT_DE = """<kontext>
+    Der Benutzer interagiert mit dem Kopiloten mit einfachen Fragen, Mehrparteienfragen, Folgefragen usw. Einfache Fragen müssen in **Anweisungen** umformuliert werden, um die semantische Suche in einer Vektordatenbank zu verbessern. Mehrteilige Fragen sollten in einfache Unteranweisungen zerlegt werden, die pro Unteranweisung eine klare Idee/ein klares Konzept enthalten. Folgefragen sollten umformuliert werden, um den Kontext des Gesprächs und die letzten Instruktionen/Informationen des Nutzers zu berücksichtigen.
+</kontext>.
+
+<ziel>
+    Fragen schreiben, die unter Berücksichtigung des Kontexts der Konversation und der letzten Mitteilungen des Nutzers in Anweisungen umformuliert werden, um die semantische Suche zu verbessern.
+</ziel>
+
+<anweisungen>
+    <anweisung>Wenn die <frage> und der <gesprächsgeschichte> gegeben sind, formuliere Anweisungen, die den Kontext der Konversation und die letzten Nachrichten des Benutzers berücksichtigen</anweisung>
+    <anweisung>Wenn die Frage in mehrere Unteranweisungen zerlegt werden kann, formuliere jede Unteranweisung separat neu<anweisung>
+    <anweisung>Jede Unteranweisung sollte nur eine Idee oder ein Konzept enthalten<anweisung>
+    <anweisung>Wenn die <frage> nur Präzisierungen nach einer vorherigen Frage und der Antwort des Assistenten enthält, sehen Sie sich den jüngsten <gesprächsgeschichte> an, um relevante Fragen für eine Neuformulierung zu extrahieren/abzuleiten<anweisung>
+    <anweisung>Berücksichtigen Sie den gesamten neueren <gesprächsgeschichte>, der mit der <frage> in Zusammenhang steht, um die Neuformulierung zu verfeinern<anweisung>
+    <anweisung>Schreiben Sie {n_alt_queries} umformulierte Fragen als Anweisung in der gleichen Sprache wie die <frage><anweisung>
 </anweisungen>
 
 <format_der_antwort>
@@ -118,10 +154,19 @@ Was ändert sich mit avs21 und wann ist die Reform in Kraft getreten? -> ["Ände
 </frage>"""
 
 
-QUERY_STATEMENT_REWRITING_PROMPT_FR = """<instructions>
-    <instruction>Étant donné la <question> ci-dessous, reformulez-la en plusieurs énoncés sous forme d'instruction</instruction>
+QUERY_STATEMENT_REWRITING_PROMPT_FR = """<context>
+    L'utilisateur intéragit avec le copilote avec des questions simples, des questions multiparties, des questions de suivi, etc. Les questions simples doivent être reformulée en **instructions** pour améliorer la recherche sémantique dans une base de données vectorielle. Les questions multiparties doivent être décomposées en plusieurs sous-instructions simples contenant une idée/concept clair par sous-instruction. Les questions de suivi doivent être reformulées pour prendre en compte le contexte de la conversation et les dernières intructions/informations de l'utilisateur.
+</context>
+
+<objectif>
+    Ecrire des questions reformulée en instructions en fonction du contexte de la conversation et des derniers messages de l'utilisateur, afin d'améliorer la recherceh sémantique.
+</objectif>
+
+<instructions>
+    <instruction>Étant donné la <question> et l'<historique_de_conversation>, formulez des instructions prenant en compte le contexte de la conversation et des derniers messages de l'utilisateur</instruction>
     <instruction>Quand la question peut être décomposée en plusieurs sous-instructions, reformulez chaque sous-instruction séparément</instruction>
     <instruction>Chaque sous-instruction doit contenir une seule idée ou concept</instruction>
+    <instruction>Si la <question> ne contient que des précisions suite à une question précédente et la réponse de l'assistant, consultez l'<historique_de_conversation> récent afin d'extraire/déduire des questions pertinentes à reformuler</instruction>
     <instruction>Prenez en compte tout l'<historique_de_conversation> récent et en rapport avec la <question> afin de raffiner la reformulation</instruction>
     <instruction>Écrire {n_alt_queries} questions reformulées sous forme d'instruction dans la même langue que la <question></instruction>
 </instructions>
@@ -149,11 +194,20 @@ Qu'est-ce qui change avec avs21 et quand la réforme est-elle entrée en vigueur
 </question>"""
 
 
-QUERY_STATEMENT_REWRITING_PROMPT_IT = """<istruzioni>
-    <istruzione> Data la <domanda> qui sotto, riformularla in diverse affermazioni sotto forma di istruzione</istruzione>
-    <istruzione>Quando la domanda può essere scomposta in diverse sotto-istruzione, riformulare ogni sotto-istruzione separatamente</istruzione>
-    <istruzione>Ogni sotto-istruzione deve contenere una singola idea o concetto</istruzione>
-    <istruzione>Tenere conto di tutte le <memoria_conversazionale> recenti e relative alla <domanda> per perfezionare la riformulazione</istruzione>
+QUERY_STATEMENT_REWRITING_PROMPT_IT = """<contesto>
+    L'utente interagisce con il copilota con domande semplici, domande multiple, domande di follow-up, ecc. Le domande semplici devono essere riformulate in **istruzioni** per migliorare la ricerca semantica in un database vettoriale. Le domande in più parti devono essere suddivise in diverse semplici sottoistruzioni contenenti un'idea/concetto chiaro per ogni sottoistruzione. Le domande successive devono essere riformulate per tenere conto del contesto della conversazione e delle ultime informazioni dell'utente.
+</contesto>
+
+<obiettivo>
+    Scrivere domande riformulate in istruzioni in base al contesto della conversazione e agli ultimi messaggi dell'utente, per migliorare la ricerca semantica.
+</obiettivo>
+
+<istruzioni>
+    <istruzione>Data la <domanda> e la <memoria_conversazionale>, formulare istruzioni che tengano conto del contesto della conversazione e dei messaggi più recenti dell'utente</istruzione>
+    <istruzione>Quando la domanda può essere suddivisa in più sottoistruzioni, riformulare ogni sottoistruzione separatamente</istruzione>
+    <istruzione>Ogni sottoistruzione deve contenere una singola idea o concetto</istruzione>
+    <istruzione>Se la <domanda> contiene solo chiarimenti dopo una domanda precedente e la risposta della procedura guidata, consultare la <memoria_conversazionale> recente per estrarre/ridurre le domande pertinenti da riformulare</istruzione>
+    <istruzione>Considerare tutta la <memoria_conversazionale> recente e pertinente alla <domanda> per perfezionare la riformulazione</istruzione>
     <istruzione>Scrivere {n_alt_queries} domande riformulate come istruzione nella stessa lingua della <domanda></istruzione>
 </istruzioni>
 
